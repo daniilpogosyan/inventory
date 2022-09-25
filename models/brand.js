093980af144440398602da97becd83fa6f33aa19
Daniil Pogosyan <daniilpogosyan@gmail.com>
2022-09-25 18:36:05 +0500

    Create model Brand

---
+++ models/brand.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const BrandSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+  }
+});
+
+module.exports = mongoose.model('Brand', BrandSchema);
