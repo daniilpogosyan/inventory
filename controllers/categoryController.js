07f84fffb39373601a37731f2fab775d2981f1eb
Daniil Pogosyan <daniilpogosyan@gmail.com>
2022-09-30 12:28:12 +0500

    Create form for creating categories

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,3 +1,6 @@
+const { body, validationResult } = require('express-validator');
+
+
 const Category = require('../models/category');
 
 
@@ -13,3 +16,34 @@ exports.categoryListGET = (req, res, next) => {
     });
   });
 }
+
+exports.categoryFormGET = (req, res, next) => {
+  res.render('category-form', {
+    title: "New Category",
+  })
+}
+
+exports.categoryFormPOST = [
+  body('name')
+    .trim()
+    .isLength({min: 3, max: 50})
+    .escape()
+    .withMessage('"Name" length should be from 3 to 50 characters'),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return next(errors.array());
+    }
+
+    const category = new Category({
+      name: req.body.name
+    });
+    category.save((err, category) => {
+      if (err) {
+        return next(err)
+      }
+      
+      res.redirect(category.url)
+    })
+  }
+]
--- routes/catalog.js
+++ routes/catalog.js
@@ -11,6 +11,12 @@ router.get('/brands', brandController.brandListGET);
 // Get list of categories
 router.get('/categories', categoryController.categoryListGET);
 
+// Get the form for creating category
+router.get('/category/create', categoryController.categoryFormGET);
+
+// Create category from the form data
+router.post('/category/create', categoryController.categoryFormPOST);
+
 // Get list of items
 router.get('/items', itemController.itemListGET);
 
